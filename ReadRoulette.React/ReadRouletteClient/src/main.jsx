f2d109e400ad53e164b856444affcb57824d6862
JustWaveThings <justwavethings@proton.me>
2024-02-23 15:41:43 -0500

    refactor router to use createRoutesFromElements instead of children

--- ReadRoulette.React/ReadRouletteClient/src/main.jsx
+++ ReadRoulette.React/ReadRouletteClient/src/main.jsx
@@ -1,25 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Layout from './Layout.jsx'
 import NotFound from './NotFound.jsx'
+import Home from './Home.jsx'
 import {
-    createBrowserRouter,
+    createBrowserRouter, createRoutesFromElements, Route,
     RouterProvider,
 } from "react-router-dom";
 import './index.css'
 
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>
-    },
-    {
-        path: "*",
-        element: <NotFound/>
-    }
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            path="/"
+            element={<Layout />}
+            errorElement={<h1>Error</h1>}
+            >
+            <Route
+                path="home"
+                element={<App/>}
+            />
+            <Route
+                path="login"
+                element={<h1>Login</h1>}
+            />
+            <Route
+                path="dashboard"
+                element={<h1>Dashboard</h1>}
+            />
+            <Route
+                path="*"
+                element={<NotFound />}
+            />
+        </Route>
 
-])
+))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
