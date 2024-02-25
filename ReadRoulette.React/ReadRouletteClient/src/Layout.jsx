e295204784f22b36d47f92a1c4c351da4c0fe013
JustWaveThings <justwavethings@proton.me>
2024-02-25 18:35:26 -0500

    create header component

--- ReadRoulette.React/ReadRouletteClient/src/Layout.jsx
+++ ReadRoulette.React/ReadRouletteClient/src/Layout.jsx
@@ -1,17 +1,17 @@
 import {Outlet, Link} from "react-router-dom";
+import Header from "./Header.jsx";
 
 function Layout() {
     return (
         <div>
+            <Header/>
             <nav>
                 <Link to="home">Home</Link>
                 <Link to="login">Login</Link>
                 <Link to="signup">Signup</Link>
                 <Link to="dashboard">Dashboard</Link>
             </nav>
-            <div>
-                <Outlet />
-            </div>
+            <Outlet/>
         </div>
     )
 }
