1032e7d0879784390f7e297b5d715b040eeac482
JustWaveThings <justwavethings@proton.me>
2024-02-25 18:43:13 -0500

    add footer component

--- ReadRoulette.React/ReadRouletteClient/src/Layout.jsx
+++ ReadRoulette.React/ReadRouletteClient/src/Layout.jsx
@@ -1,5 +1,6 @@
 import {Outlet, Link} from "react-router-dom";
 import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 
 function Layout() {
     return (
@@ -12,6 +13,7 @@ function Layout() {
                 <Link to="dashboard">Dashboard</Link>
             </nav>
             <Outlet/>
+            <Footer/>
         </div>
     )
 }
