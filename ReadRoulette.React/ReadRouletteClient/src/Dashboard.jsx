ce1fe5e00a5c2615740c64faee0556e58bf5662d
JustWaveThings <justwavethings@proton.me>
2024-02-26 11:27:37 -0500

    add nav and styling

--- ReadRoulette.React/ReadRouletteClient/src/Dashboard.jsx
+++ ReadRoulette.React/ReadRouletteClient/src/Dashboard.jsx
@@ -1,8 +1,12 @@
-import { Outlet} from "react-router-dom";
+import { Outlet, Link} from "react-router-dom";
 export default function Dashboard(){
     return (
-        <div className="main">
+        <div className="main dashboard">
             <h1>Dashboard</h1>
+            <nav className="dash-nav">
+                <Link to="/dashboard">Profile</Link>
+                <Link to="booklist">Book List</Link>
+            </nav>
             <Outlet/>
         </div>
 
