cb3aadc78c5cd1e342dba26556dca2785eca9e59
JustWaveThings <justwavethings@proton.me>
2024-02-26 09:36:58 -0500

    add dashboard outlet routes

--- ReadRoulette.React/ReadRouletteClient/src/main.jsx
+++ ReadRoulette.React/ReadRouletteClient/src/main.jsx
@@ -19,40 +19,87 @@ const router = createBrowserRouter(
         <Route
             path="/"
             element={<Layout />}
-            // errorElement={<h1>Error</h1>}
+            errorElement={<h1>Error</h1>}
             >
             <Route
                 index
                 element={<App/>}
-
+                errorElement={<h1>Error</h1>}
             />
             <Route
                 path="home"
                 element={<App/>}
-
+                errorElement={<h1>Error</h1>}
             />
             <Route
                 path="login"
                 element={<Login />}
-
+                errorElement={<h1>Error</h1>}
             />
             <Route
                 path="signup"
                 element={<Signup/>}
+                errorElement={<h1>Error</h1>}
+            />
+            <Route
+                path="*"
+                element={<NotFound />}
+                errorElement={<h1>Error</h1>}
+
             />
             <Route
                 path="dashboard"
                 element={<Dashboard/>}
                 loader={async ({request}) => await requireAuth(request)}
+                errorElement={<h1>Error</h1>}
+            >
+                <Route
+                    index
+                    element={<h1>Profile</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path="settings"
+                    element={<h1>Settings</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path="booklist"
+                    element={<h1>Your BookList</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path="bookclubs"
+                    element={<h1>All BookClubs</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path="bookclubs/:id"
+                    element={<h1>BookClub</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path="bookclubs/:id/members"
+                    element={<h1>Members</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path="bookclubs/:id/books"
+                    element={<h1>Book Club Book List</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path="bookclubs/:id/books/:id"
+                    element={<h1>Book Club Book List Specific Book</h1>}
+                    errorElement={<h1>Error</h1>}
+                />
+                <Route
+                    path= "*"
+                    element={<NotFound/>}
+                    errorElement={<h1>Error</h1>}
+                />
 
-            />
-            <Route
-                path="*"
-                element={<NotFound />}
-
-            />
-
-
+            </Route>
         </Route>
 
 ))
