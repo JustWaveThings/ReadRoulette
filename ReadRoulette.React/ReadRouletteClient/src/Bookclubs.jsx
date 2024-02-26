56022204f9ff66ecdee88ba7a027e792f34b02d8
JustWaveThings <justwavethings@proton.me>
2024-02-26 10:10:53 -0500

    add component

---
+++ ReadRoulette.React/ReadRouletteClient/src/Bookclubs.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+export default function BookClubs() {
+    return
+    (
+        <>
+            <h1>Book Clubs</h1>
+            <Outlet/>
+        </>
+    )
+
+}
