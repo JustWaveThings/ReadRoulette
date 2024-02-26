4750ab344fd277168a1eda6e8505def911a415ff
JustWaveThings <justwavethings@proton.me>
2024-02-25 19:25:47 -0500

    fake auth boolean for testing

--- ReadRoulette.React/ReadRouletteClient/src/requireAuth.js
+++ ReadRoulette.React/ReadRouletteClient/src/requireAuth.js
@@ -2,7 +2,8 @@ import {redirect} from "react-router-dom";
 
 export default async function requireAuth(request){
     const pathName = new URL(request.url);
-    const isLoggedIn = localStorage.getItem('isLoggedInRoulette');
+    //const isLoggedIn = localStorage.getItem('isLoggedInRoulette');
+    const isLoggedIn = true;
     if(!isLoggedIn){
         throw redirect(`/login?redirectTo=${pathName.pathname}`);
     }
