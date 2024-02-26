5731c562a6ebfa8f6f1b557d5c7c5a562dd3f2b3
JustWaveThings <justwavethings@proton.me>
2024-02-26 11:36:05 -0500

    add nav and styling

--- ReadRoulette.React/ReadRouletteClient/src/BookList.jsx
+++ ReadRoulette.React/ReadRouletteClient/src/BookList.jsx
@@ -1,9 +1,13 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, Link} from "react-router-dom";
 
 export default function BookList(){
     return (
         <>
             <h1>Book List</h1>
+            <nav className="booklist-nav">
+                <Link to="/dashboard/booklist">All Books</Link>
+                <Link to="managebooklist">Manage Book List</Link>
+            </nav>
             <Outlet/>
         </>
         )
